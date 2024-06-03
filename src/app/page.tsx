07cf196181e7299549e2942d35f4613970d8d2d1
Sanket <sanketyelave11@gmail.com>
2024-06-03 15:47:57 +0530

    Basic Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24">
       Koi bhi iss page me kuch bhi push nahi karega. <br />
       Agar apna banaya hua page dekhna hai then just go to " http://localhost:3000/prototype/your-name"
-      and ya port alag ho sakta hai hence dekh kar.
+      and ya port alag ho sakta hai hence dekh kar. <br />
+      Do checkout http://localhost:3000/prototype/sanket
+
 
 
     </main>
