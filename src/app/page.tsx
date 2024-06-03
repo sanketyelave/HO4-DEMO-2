9469c9ab99bdd82c046d5147b0059a0272a86fa8
Sanket <sanketyelave11@gmail.com>
2024-06-03 15:44:56 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      Koi bhi iss page me kuch bhi push nahi karega. <br />
+      Agar apna banaya hua page dekhna hai then just go to " http://localhost:3000/prototype/your-name"
+      and ya port alag ho sakta hai hence dekh kar.
+
+
+    </main>
+  );
+}
---
+++ src/app/prototype/sanket/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+    return (
+        <div>
+            Hii juniors sanket this side and if can navigate till here then congo 🎉 u know how to do routhing in nextjs like atleast basic routhing agar nexting seekhni ho then do read docs ya ask anyome of us.
+        </div>
+    );
+}
+
+export default page;
